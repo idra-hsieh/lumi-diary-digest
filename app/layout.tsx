ba3c744fe99ce93e9f03058bd5b3874008fd4ad7
Idra Hsieh <idra.hsieh@gmail.com>
2025-11-14 17:02:42 +0800

    feat: install shadcn and its theme provider, toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./styles/globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Lumi Diary Digest",
@@ -11,8 +13,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>Lumi Diary Digest</body>
+    <html lang="en" suppressHydrationWarning>
+      <body><ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider></body>
     </html>
   );
 }
