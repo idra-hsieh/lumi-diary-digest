58eb1ca59dabf44bc208f530412477c9e2dd150c
Idra Hsieh <idra.hsieh@gmail.com>
2025-11-19 21:20:05 +0800

    feat: add search bar to side bar

--- components/SidebarGroupContent.tsx
+++ components/SidebarGroupContent.tsx
@@ -1,14 +1,30 @@
 "use client";
 
 import { Diary } from "@/types/prisma";
+import { SidebarContent as SidebarGroupContentShadCN } from "./ui/sidebar";
+import { SearchIcon } from "lucide-react";
+import { Input } from "./ui/input";
+import { useState } from "react";
 
 type Props = {
   diaries: Diary[];
 };
 
 function SidebarGroupContent({ diaries }: Props) {
-  console.log(diaries);
-  return <div>Your diaries here</div>;
+  const [searchText, setSearchText] = useState("");
+  return (
+    <SidebarGroupContentShadCN>
+      <div className="relative flex items-center">
+        <SearchIcon className="absolute left-2 size-2" />
+        <Input
+          className="bg-muted pl-8"
+          placeholder="Search your diary..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+    </SidebarGroupContentShadCN>
+  );
 }
 
 export default SidebarGroupContent;
