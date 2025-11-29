52eebbc2e250004af5427d00c3508b14d773cfc6
Idra Hsieh <idra.hsieh@gmail.com>
2025-11-29 20:31:43 +0800

    fix: remove proxy default export

--- proxy.ts
+++ proxy.ts
@@ -5,8 +5,6 @@ export async function proxy(request: NextRequest) {
   return updateSession(request);
 }
 
-export default proxy;
-
 export const config = {
   matcher: [
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
