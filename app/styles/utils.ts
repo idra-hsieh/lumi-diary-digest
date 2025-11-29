2cdc5f05d85979a42efcf1064842127b34289f4b
Idra Hsieh <idra.hsieh@gmail.com>
2025-11-29 22:09:03 +0800

    style: adjust globals.css color palette for more exquisite ui

--- app/styles/utils.ts
+++ app/styles/utils.ts
@@ -1,3 +1,3 @@
-const glowColor = "rgba(27, 65, 74, 0.4)";
+const glowColor = "rgba(47, 91, 104, 0.35)";
 
 export const shadow = `0 0 5px ${glowColor}, 0 0 10px ${glowColor}, 0 0 15px ${glowColor}, 0 0 20px ${glowColor}`;
